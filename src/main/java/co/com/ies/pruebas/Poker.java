bc291b337d8997b7e1e6c57663f0b2773da9c68f
luis.martinez10 <luis.martinez@iesonline.com.co>
2021-10-02 12:48:23 -0500

    Se aggregan nuevos cambios

--- src/main/java/co/com/ies/pruebas/Poker.java
+++ src/main/java/co/com/ies/pruebas/Poker.java
@@ -2,6 +2,39 @@ package co.com.ies.pruebas;
 
 public class Poker {
     public WinnerRound playRound(PokerHand blanco, PokerHand negro) {
-        return null;
+        WinnerRound ganador = new WinnerRound();
+        String[] whiteHand = blanco.toString().split(" ");
+        String[] blackHand = blanco.toString().split(" ");
+        int maxWhite = 0;
+        int maxBlack = 0;
+        String[] numerosCartas = {"2","3","4","5","6","7","8","9","10", "J", "Q", "K", "A"};
+        for(int i = 0; i < whiteHand.length; i++){
+            String carta = whiteHand[i];
+            int numCarta = (int)carta.charAt(0);
+            String cartaSiguiente = whiteHand[i+1];
+            int numCartaSiguiente = (int)cartaSiguiente.charAt(0);
+            if(numCarta > numCartaSiguiente){
+                maxWhite = numCarta;
+            } else {
+                maxWhite = numCartaSiguiente;
+            }
+        }
+
+        for(int i = 0; i < blackHand.length; i++){
+            String carta = blackHand[i];
+            int numCarta = (int)carta.charAt(0);
+            String cartaSiguiente = blackHand[i+1];
+            int numCartaSiguiente = (int)cartaSiguiente.charAt(0);
+            if(numCarta > numCartaSiguiente){
+                maxBlack = numCarta;
+            } else {
+                maxBlack = numCartaSiguiente;
+            }
+        }
+
+        if(maxWhite > maxBlack){
+            
+        }
+        return ganador;
     }
 }
